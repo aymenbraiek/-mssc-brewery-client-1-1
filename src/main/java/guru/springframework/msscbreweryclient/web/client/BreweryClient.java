418a3dc08eaffda67aad0ab9ea6d44664df2e0a8
aymen braiek <aymen.braiek@esprit.tn>
2021-03-23 22:22:45 +0100

    adding HTTP POST Spring Boot -closes #4

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -6,12 +6,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 @Component
 public class BreweryClient {
-    ///api/v1/beer/ spring boot projet controller getmapping
+    ///api/v1/beer/ spring boot project controller getmapping URL
     public final String BEER_PATH_V1 = "/api/v1/beer/";
     private String apihost;
 
@@ -21,9 +22,15 @@ public class BreweryClient {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public BeerDto getBeerById(UUID uuid){
+    // HTTP GET Spring Boot Rest Template
+    public BeerDto getBeerById(UUID uuid) {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
     }
+    // HTTP POST Spring Boot Rest Template
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
 
     public void setApihost(String apihost) {
         this.apihost = apihost;
