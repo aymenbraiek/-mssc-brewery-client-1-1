49c6a5d9ab37803e22f8a2239f900f5cf809ea06
aymen braiek <aymen.braiek@esprit.tn>
2021-03-23 21:06:35 +0100

    addling HTTP POST Test Rest Template -closes #3

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -15,7 +15,7 @@ public class BreweryClientTest {
 
     @Autowired
     BreweryClient client;
-
+// HTTP GET Spring Boot Rest Template
     @Test
     void getBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
@@ -23,4 +23,11 @@ public class BreweryClientTest {
         assertNotNull(dto);
 
     }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+
+
 }
